956f3116db7c27598047680553bd7d8befbf0e47
szage07 <cjbuquis@gmail.com>
2024-01-14 11:32:33 +0800

    2nd vid done on backend-laravel

---
+++ app/Models/Carousel.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Carousel extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'carousel';
+
+     /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'carousel_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'carousel_name',
+        'image_path',
+        'description',
+        'user_id',
+
+    ];
+}
---
+++ database/migrations/2024_01_14_023657_create_carousel_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carousel', function (Blueprint $table) {
+            $table->id('carousel_id');
+            $table->string('carousel_name')->nullable();
+            $table->string('image_path');
+            $table->string('description')->nullable();
+            $table->timestamps();
+
+            
+        });
+        Schema::table('carousel', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+         
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carousel');
+    }
+};
