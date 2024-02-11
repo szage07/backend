862fc8d03308428a6c944412c6d64e0e99c9781d
szage07 <cjbuquis@gmail.com>
2024-02-11 19:05:57 +0800

    Backend - Laravel Part 11 - Laravel Sanctum and Composer Discussion done

--- routes/api.php
+++ routes/api.php
@@ -29,10 +29,10 @@ Route::put('/carousel/{id}',[CarouselController::class, 'update']);
 Route::delete('/carousel/{id}',[CarouselController::class, 'destroy']); 
 
 
-Route::get('/user',[UserController::class, 'index']); 
-Route::delete('/user/{id}',[UserController::class, 'destroy']);
-Route::get('/user/{id}',[UserController::class, 'show']); 
-Route::put('/user/{id}',[UserController::class, 'update'])->name('user.update');
-Route::put('/user/email/{id}',[UserController::class, 'email'])->name('user.email');
-Route::put('/user/password/{id}',[UserController::class, 'password'])->name('user.password');
-Route::post('/user',[UserController::class, 'store'])->name('user.store'); 
+// Route::get('/user',[UserController::class, 'index']); 
+// Route::delete('/user/{id}',[UserController::class, 'destroy']);
+// Route::get('/user/{id}',[UserController::class, 'show']); 
+// Route::put('/user/{id}',[UserController::class, 'update'])->name('user.update');
+// Route::put('/user/email/{id}',[UserController::class, 'email'])->name('user.email');
+// Route::put('/user/password/{id}',[UserController::class, 'password'])->name('user.password');
+// Route::post('/user',[UserController::class, 'store'])->name('user.store'); 
